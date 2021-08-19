79e9c4196372ad95d03bfcee4ef5192917a3b90f
imSaharukh <sasaharukh@gmail.com>
2021-08-19 14:49:12 +0600

    wip: product validate

--- SuperShop.App/UI/Product/AddProductForm.cs
+++ SuperShop.App/UI/Product/AddProductForm.cs
@@ -18,9 +18,47 @@ namespace SuperShop.App
             InitializeComponent();
         }
 
+
+        bool validate()
+        {
+            bool result = true;
+            //if (string.IsNullOrEmpty(this.txtFirstName.Text))
+            //{
+
+            //    result = false;
+            //    this.lblErrFirstName.Text = "First Name can't be empty";
+            //    this.lblErrFirstName.Visible = true;
+            //    //Console.WriteLine("inside if");
+            //}
+            //if (string.IsNullOrEmpty(this.txtUsername.Text))
+            //{
+
+            //    result = false;
+            //    this.lblErrUname.Text = "User Name can't be empty";
+            //    this.lblErrUname.Visible = true;
+            //    //Console.WriteLine("inside if");
+            //}
+            //if (string.IsNullOrEmpty(this.txtPassword.Text))
+            //{
+
+            //    result = false;
+            //    this.lblErrPassword.Text = "Password can't be empty";
+            //    this.lblErrPassword.Visible = true;
+            //    //Console.WriteLine("inside if");
+            //}
+            //if (string.IsNullOrEmpty(this.txtLastName.Text))
+            //{
+
+            //    result = false;
+            //    this.lblErrLastName.Text = "Last Name can't be empty";
+            //    this.lblErrLastName.Visible = true;
+            //    //Console.WriteLine("inside if");
+            //}
+            return result;
+        }
         private void AddProductForm_Load(object sender, EventArgs e)
         {
-
+            this.lblErrName.Visible = false;
         }
     }
 }
